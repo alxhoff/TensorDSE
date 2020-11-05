aebdf67dc62778ab333aa2b68f8c9ae136809842
alafnayou <afnayou@gmail.com>
2020-11-05 20:36:58 +0100

    Model created,trained and saved as test_model.h5

---
+++ benchmarking/splitting_tflite_model/test_model_creation.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from tensorflow import keras
+print(tf.__version__)
+print(keras.__version__)
+
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+
+print(X_train_full.shape)
+print(X_train_full.dtype)
+
+X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
+y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
+X_test = X_test / 255.0
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
+               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
+
+print(class_names[y_train[0]])
+
+model = keras.models.Sequential([
+    keras.layers.Flatten(input_shape=[28, 28]),
+    keras.layers.Dense(300, activation="relu"),
+    keras.layers.Dense(100, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.summary()
+
+hidden1 = model.layers[1]
+weights, biases = hidden1.get_weights()
+
+model.compile(loss="sparse_categorical_crossentropy",
+              optimizer="sgd",
+              metrics=["accuracy"])
+
+history = model.fit(X_train, y_train, epochs=30,
+                    validation_data=(X_valid, y_valid))
+
+model.save("test_model.h5")
