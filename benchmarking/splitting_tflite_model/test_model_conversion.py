69ec4a46dfc2517e6d1dd714d44134cf20dcb74d
alafnayou <afnayou@gmail.com>
2020-11-06 22:45:51 +0100

    Adjusted model, full-integer post training quantization successful, tflite model generated

--- benchmarking/splitting_tflite_model/test_model_conversion.py
+++ benchmarking/splitting_tflite_model/test_model_conversion.py
@@ -1,21 +1,36 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-print(tf.__version__)
 
-#model = keras.models.load_model("test_model.h5")
+model = tf.keras.models.load_model('../splitting_tflite_model/saved_test_model')
 
-#converter = tf.lite.TFLiteConverter.from_keras_model(model)
-#converter.optimizations = [tf.lite.Optimize.DEFAULT]
-#def representative_dataset_gen():
-#  for _ in range(num_calibration_steps):
-    # Get sample input data as a numpy array in a method of your choosing.
-#    yield [input]
-#converter.representative_dataset = representative_dataset_gen
-#converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
-#converter.inference_input_type = tf.int8  # or tf.uint8
-#converter.inference_output_type = tf.int8  # or tf.uint8
-#tflite_quant_model = converter.convert()
+fashion_mnist = keras.datasets.fashion_mnist
+(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
 
-#with open('test_model.tflite', 'wb') as f:
-#  f.write(tflite_test_model)
+
+X_train = X_train_full.astype(np.float32) / 255.0
+y_train = y_train_full
+X_test = X_test.astype(np.float32) / 255.0
+
+
+def representative_dataset_gen():
+  for input_value in tf.data.Dataset.from_tensor_slices(X_train).batch(1).take(100):
+    yield [input_value]
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+converter.representative_dataset = representative_dataset_gen
+converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+converter.inference_input_type = tf.uint8  # or tf.uint8
+converter.inference_output_type = tf.uint8  # or tf.uint8
+
+tflite_quant_model = converter.convert()
+
+interpreter = tf.lite.Interpreter(model_content=tflite_quant_model)
+input_type = interpreter.get_input_details()[0]['dtype']
+print('input: ', input_type)
+output_type = interpreter.get_output_details()[0]['dtype']
+print('output: ', output_type)
+
+with open('test_model.tflite', 'wb') as f:
+  f.write(tflite_quant_model)
--- benchmarking/splitting_tflite_model/test_model_creation.py
+++ benchmarking/splitting_tflite_model/test_model_creation.py
@@ -1,37 +1,35 @@
+import logging
+logging.getLogger("tensorflow").setLevel(logging.DEBUG)
+
 import tensorflow as tf
+import numpy as np
 from tensorflow import keras
 
 
-fashion_mnist = keras.datasets.fashion_mnist
-(X_train_full, y_train_full), (X_test, y_test) = fashion_mnist.load_data()
+mnist = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 
-X_valid, X_train = X_train_full[:5000] / 255.0, X_train_full[5000:] / 255.0
-y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
-X_test = X_test / 255.0
+train_images = train_images.astype(np.float32) / 255.0
+test_images = test_images.astype(np.float32) / 255.0
 
-class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat",
-               "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
-print(class_names[y_train[0]])
 
 model = keras.models.Sequential([
-    keras.layers.Flatten(input_shape=[28, 28]),
-    keras.layers.Dense(300, activation="relu"),
-    keras.layers.Dense(100, activation="relu"),
-    keras.layers.Dense(10, activation="softmax")
+    keras.layers.InputLayer(input_shape=(28, 28)),
+    keras.layers.Reshape(target_shape=(28,28,1)),
+    keras.layers.Conv2D(filters=12, kernel_size=(3, 3), activation='relu'),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Flatten(),
+    keras.layers.Dense(10)
 ])
 
-model.summary()
-
-hidden1 = model.layers[1]
-weights, biases = hidden1.get_weights()
 
 model.compile(loss="sparse_categorical_crossentropy",
-              optimizer="sgd",
+              optimizer="adam",
               metrics=["accuracy"])
 
-history = model.fit(X_train, y_train, epochs=30,
-                    validation_data=(X_valid, y_valid))
+history = model.fit(train_images, train_labels, epochs=5,
+                    validation_data=(test_images, test_labels))
 
-model.save("test_model.h5")
+model.save('saved_test_model')
