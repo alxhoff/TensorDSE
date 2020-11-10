35754ca17731e7112f7ae91d65b21c8a01bc3404
alafnayou <afnayou@gmail.com>
2020-11-10 17:43:18 +0100

    manipulating JSON file in progress, needs further fixing

--- benchmarking/splitting_tflite_model/mnist_tflite_model/test_model_splitting.py
+++ benchmarking/splitting_tflite_model/mnist_tflite_model/test_model_splitting.py
@@ -24,12 +24,13 @@ def split_edgetpu_model(log: logging.Logger, name: str):
     conv_opcode = -1
     new_opcodes = []
     for i, c in enumerate(model["operator_codes"]):
-        if c["builtin_code"] == "DEPTHWISE_CONV_2D":
+        if c["deprecated_builtin_code"] == 4:
             new_opcodes.append(c)
             conv_opcode = i
-    #assert conv_opcode >= 0
+    assert conv_opcode >= 0
     model["operator_codes"] = new_opcodes
     print(new_opcodes)
+    print('\n')
 
     # Fix the tensor dtypes which are int8 instead of uint8.
     # Also remove the multi-channel quantization which is not supported on Edge TPU.
@@ -47,9 +48,12 @@ def split_edgetpu_model(log: logging.Logger, name: str):
         #t["quantization"]["quantized_dimension"] = 0
         index_map[i] = len(new_tensors)
         new_tensors.append(t)
+        print(t)
+        print('\n')
     graph["tensors"] = new_tensors
+    
 
-    # Update the tensor indexes in the ops.
+    """# Update the tensor indexes in the ops.
     new_ops = []
     for op in graph["operators"]:
         if op["opcode_index"] != conv_opcode:
@@ -59,15 +63,16 @@ def split_edgetpu_model(log: logging.Logger, name: str):
         new_ops.append(op)
     graph["operators"] = new_ops
 
+
     
     
     # Update the global input and output tensor indexes.
     graph["inputs"][0] = new_ops[0]["inputs"][0]
     graph["outputs"][0] = new_ops[0]["outputs"][0]
-    model["subgraphs"][0] = graph
+    model["subgraphs"][0] = graph"""
 
-    with open(fn_json, "w") as fout:
-        json.dump(model, fout, indent=4)
+    #with open(fn_json, "w") as fout:
+        #json.dump(model, fout, indent=4)
     
     #log.info("Generating the binary flatbuffers model from JSON")
     #echo_run("flatc", "-b", "schema.fbs", fn_json)
