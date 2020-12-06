ea9db721e0a1a3c0e6eb0c7111c1ee60d932eae8
alafnayou <afnayou@gmail.com>
2020-12-06 23:37:13 +0100

    Code structuring in progress, echo_run function implementation successful

--- manipulating_tflite_models/main.py
+++ manipulating_tflite_models/main.py
@@ -1,26 +1,43 @@
-source_model_filename = "models/source_model/MNIST_model.tflite"
+import os
+import json
+import logging
+from pathlib import Path
 
-def tflite_model_optimization():
+import utils
 
-    #log.info("Patching the model in JSON")
-    fn_json = str(Path(source_model_filename).with_suffix(".json"))
+models_dir = "./models/source_model/"
 
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    fn_json = os.path.join(dir_path, fn_json)
+def check_dir(models_dir: str):
+    for file in os.listdir(models_dir):
+        if file.endswith(".tflite"):
+            source_model_filename = file
+            source_model_filepath = os.path.join(models_dir, file)
+    
+    return source_model_filename,source_model_filepath
 
-    with open(fn_json) as fin:
-        model = json.load(fin)
+def tflite_model_optimization(log: logging.Logger, source_model_filepath: str):
+
+    log.info("Patching the original model in JSON...")
+    source_model_filepath_json = str(Path(source_model_filepath).with_suffix(".json"))
+
+    with open(source_model_filepath_json) as fin:
+        original_model = json.load(fin)
+    
+    log.info("Creating tmp files...")
+    utils.echo_run("mkdir", "models/submodels/tmp_folder")
+    if not Path("models/submodels/tmp_folder").exists():
+        print("fuck")
+    else:
+        print("yes")
+        utils.echo_run("rm", "-rf", "models/submodels/tmp_folder")
 
 if __name__ == '__main__':
+    
+    source_model_filename,source_model_filepath = check_dir(models_dir)
 
-    import os
-    import logging
-    from pathlib import Path
-    import subprocess
-    import json
-    import urllib.request
-    from enum import Enum
+    log = logging.getLogger(source_model_filename)
+    logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.INFO)
 
-    tflite_model_optimization()
+    tflite_model_optimization(log, source_model_filepath)
 
     pass
---
+++ manipulating_tflite_models/utils.py
@@ -0,0 +1,10 @@
+
+def echo_run(*cmd):
+    import subprocess
+    
+    #Execute an arbitrary command and echo its output.
+    p = subprocess.run(list(cmd), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    output = p.stdout.decode()
+    if output:
+        print(output)
+    p.check_returncode()
