fb4267dc674c39579fe0f4fd1eac575a2b974f17
alafnayou <afnayou@gmail.com>
2020-12-08 00:27:46 +0100

    function for loading files improved, helper functions in progress

--- manipulating_tflite_models/main.py
+++ manipulating_tflite_models/main.py
@@ -5,39 +5,52 @@ from pathlib import Path
 
 import utils
 
-models_dir = "./models/source_model/"
+mapping = [[0,2],[1,2],[2,2],[3,0],[4,2],[5,2],[6,2]]
 
-def check_dir(models_dir: str):
-    for file in os.listdir(models_dir):
-        if file.endswith(".tflite"):
-            source_model_filename = file
-            source_model_filepath = os.path.join(models_dir, file)
+def tflite_model_optimization(log: logging.Logger, main_dir_path: str):
     
-    return source_model_filename,source_model_filepath
+    log.info("Patching original model in JSON...")
+    source_model_dir_path = os.path.join(main_dir_path, "models", "source_model")
+    original_model = utils.load_json_file(log,source_model_dir_path)
+    
+    log.info("Analyzing mapping...")
+    info = utils.info_mapping(mapping)
+    log.info("Operations to be merged have following indexes: %s" % info)
 
-def tflite_model_optimization(log: logging.Logger, source_model_filepath: str):
-
-    log.info("Patching the original model in JSON...")
-    source_model_filepath_json = str(Path(source_model_filepath).with_suffix(".json"))
-
-    with open(source_model_filepath_json) as fin:
-        original_model = json.load(fin)
+    if len(info) != 0:
+        merge_flag = True
+    else:
+        merge_flag = False
+    
+    """while merge_flag:
+        submodel = utils.initialize_submodel()
+        utils.merge_ops(original_model, submodel, info)
+        if len(info) == 0:
+            merge_flag = False
+            break
     
     log.info("Creating tmp files...")
-    utils.echo_run("mkdir", "models/submodels/tmp_folder")
-    if not Path("models/submodels/tmp_folder").exists():
-        print("fuck")
+    tmp_folder_dir = os.path.dirname(os.path.dirname(source_model_filepath))
+    tmp_folder_path = os.path.join(tmp_folder_dir, "submodels", "tmp_files")
+
+    utils.echo_run("mkdir", tmp_folder_path)
+
+    if not os.path.exists(tmp_folder_path):
+        print("no")
     else:
         print("yes")
-        utils.echo_run("rm", "-rf", "models/submodels/tmp_folder")
+        utils.echo_run("rm", "-rf", tmp_folder_path)"""
 
 if __name__ == '__main__':
     
-    source_model_filename,source_model_filepath = check_dir(models_dir)
+    main_dir_path = os.path.dirname(os.path.abspath(__file__))
+    """models_dir = os.path.join(main_dir_path, "models")
+    
+    source_model_filename,source_model_filepath = utils.check_for_source_model(models_dir)"""
 
-    log = logging.getLogger(source_model_filename)
+    log = logging.getLogger(__name__)
     logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.INFO)
 
-    tflite_model_optimization(log, source_model_filepath)
+    tflite_model_optimization(log, main_dir_path)
 
     pass
--- manipulating_tflite_models/utils.py
+++ manipulating_tflite_models/utils.py
@@ -1,3 +1,45 @@
+import logging
+
+def load_json_file(log: logging.Logger, file_path: str):
+
+    import os
+    import json
+    import urllib.request
+    from pathlib import Path
+
+    
+    tflite_file_exists = False
+    json_file_exists = False
+
+    while json_file_exists == False:
+
+        for file in os.listdir(file_path):
+            if file.endswith(".tflite"):
+                file_path_tflite = os.path.join(file_path, file)
+                tflite_file_exists = True
+            elif file.endswith(".json"):
+                file_path_json = os.path.join(file_path, file)
+                json_file_exists = True
+
+        schema_path = os.path.join(file_path,"schema.fbs")
+    
+        if tflite_file_exists:
+            if json_file_exists:
+                with open(file_path_json) as fin:
+                    model = json.load(fin)
+                    log.info("Loading of model in JSON successful")
+                    return model
+            else:
+                if not Path(schema_path).exists():
+                    log.info("schema.fbs was not found, downloading")
+                    urllib.request.urlretrieve("https://github.com/tensorflow/tensorflow/raw/master/tensorflow/lite/schema/schema.fbs",
+                                               "schema.fbs")
+                    log.info("Downloaded schema.fbs")
+                    log.info("Converting the model from binary flatbuffers to JSON")
+                    echo_run("flatc", "-t", "--strict-json", "--defaults-json", schema_path, "--", file_path_tflite)
+        else:
+            log.info("TFLite model not found.")
+            return 0
 
 def echo_run(*cmd):
     import subprocess
@@ -8,3 +50,104 @@ def echo_run(*cmd):
     if output:
         print(output)
     p.check_returncode()
+
+def info_mapping(mapping: list):
+
+    sequential_ops = []
+    sequence = []
+    sequence_flag = False
+
+    for ops in mapping:
+        if ops[1] == 2:
+            if sequence_flag == False:
+                sequence.append(ops[0])
+                sequence_flag = True
+                if ops[0] == len(mapping)-1:
+                    sequential_ops.append(sequence)
+
+            elif sequence_flag == True:
+                sequence.append(ops[0])
+                if ops[0] == len(mapping)-1:
+                    sequential_ops.append(sequence)
+        elif ops[1] != 2:
+            if sequence_flag == True:
+                sequential_ops.append(sequence)
+                sequence = []
+                sequence_flag = False
+            else:
+                continue
+    
+    return sequential_ops
+
+def seperate_ops(original_model: dict, submodel: dict, info: list):
+
+    original_graph = original_model["subgraphs"][0]
+
+    new_ops = []
+    for i,op in enumerate(original_graph["operators"]):
+        for info_op in info[0]:
+            if info_op == i:
+                new_ops.append(op)
+
+    new_opcodes = []
+    for i, op_code in enumerate(original_model["operator_codes"]):
+        for new_op in new_ops:
+            if i == new_op["opcode_index"]:
+                new_opcodes.append(op_code)
+                new_op["opcode_index"] = len(new_opcodes) - 1
+
+    new_tensors = []
+    tensor_indexes = []
+    for new_op in new_ops:
+        for i,op_input in enumerate(new_op["inputs"]):
+            if op_input in tensor_indexes:
+                continue
+            else:
+                tensor_indexes.append(op_input)
+                new_tensors.append(original_graph["tensors"][op_input])
+                new_op["inputs"][i] = len(new_tensors) - 1
+        for j, op_output in enumerate(new_op["outputs"]):
+            if op_output in tensor_indexes:
+                continue
+            else:
+                tensor_indexes.append(op_output)
+                new_tensors.append(original_graph["tensors"][op_output])
+                new_op["outputs"][j] = len(new_tensors) - 1
+    
+    new_inputs = []
+    new_outputs = []
+    new_inputs.append(new_ops[0]["inputs"][0])
+    new_outputs.append(new_ops[len(new_ops) - 1]["outputs"][0])
+
+    new_buffers = []
+    buffer_indexes = []
+    for new_tensor in new_tensors:
+        index = new_tensor["buffer"]
+        if index in buffer_indexes:
+            continue
+        else:
+            buffer_indexes.append(index)
+            new_buffers.append(original_model["buffers"][index])
+            new_tensor["buffer"] = len(new_buffers) - 1
+
+def merge_ops(original_model: dict, submodel: dict, info: list):
+
+    seperate_ops(original_model,submodel,info)
+    info.pop(0)
+    return info,submodel
+
+def check_for_source_model(models_dir: str):
+    import os
+    source_model_dir = os.path.join(models_dir,"source_model")
+    for file in os.listdir(source_model_dir):
+        if file.endswith(".tflite"):
+            source_model_filename = file
+            source_model_filepath = os.path.join(source_model_dir, file)
+    
+    return source_model_filename,source_model_filepath
+
+def copy_file(file_path: str, target_path):
+    echo_run("cp",file_path,target_path)
+
+def initialize_submodel():
+    
