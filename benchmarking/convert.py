772c5e141cc7ce1f28deb26f7caa0df3271441ac
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-09-06 18:42:22 +0200

    SplitTFLiteModel now returns a list of the splitted layer names, used by later functions for double-checking

--- benchmarking/convert.py
+++ benchmarking/convert.py
@@ -176,15 +176,20 @@ def ProcessOperation(model, graph, operator) -> None:
     ProcessLayer(operator_name, operator_options, input_tensors, output_tensors)
 
 
-def SplitTFLiteModel(model) -> None:
+def SplitTFLiteModel(model) -> list:
 
     """Splits/Processes the given as input tflite model into its individual operations.
 
     Individually calls onto the process_operation function that processes and
     eventually recreates the operation overloaded onto 'process_operation(...)'
     as 'graph.Operators(i)'.
+
+    :rtype: list of strins
+        each entry is one of the layers that compose the
+        to-be-benchmarked model
     """
     from main import log
+    layers = []
     with open(model, "rb") as f:
         # Load our model from file and create the flatbuffer parser object for the root node
         model = GetTFLiteClass("Model").GetRootAsModel(f.read(), 0)
@@ -192,8 +197,10 @@ def SplitTFLiteModel(model) -> None:
         log.info(f"Model subdivided into {graph.OperatorsLength()} operators")
 
         for operator in [graph.Operators(i) for i in range(graph.OperatorsLength())]:
+            layers.append(GetOpNameFromOperator(model=model, operator=operator))
             ProcessOperation(model, graph, operator)
 
+    return layers
 
 def ImportTFLiteModules() -> None:
 
