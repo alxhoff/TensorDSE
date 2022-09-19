9251bf636b2dc80e4bed8716e1dee9e24a51e1f4
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-09-19 12:54:40 +0200

    Adjusted to different dict format

--- benchmarking/usb/analysis.py
+++ benchmarking/usb/analysis.py
@@ -9,20 +9,19 @@ def analyze_timestamps(t:UsbTimer) -> Dict:
             "usb" : {}
     }
 
-    d["usb"]["host_submission"]    = float(t.ts_end_submission - t.ts_begin_submission)
-    d["usb"]["host_communication"] = float(t.ts_end_host_send_request - t.ts_begin_host_send_request)
-    d["usb"]["tpu_communication"]  = float(t.ts_begin_return - t.ts_end_submission)
-    d["usb"]["tpu_return"]         = float(t.ts_end_return - t.ts_end_submission)
-    d["usb"]["inference"]          = float(t.ts_begin_return - t.ts_end_submission)
-    d["usb"]["total"]              = float(t.ts_end_return - t.ts_begin_host_send_request)
+    d["host_submission"]    = float(t.ts_end_submission - t.ts_begin_submission)
+    d["host_communication"] = float(t.ts_end_host_send_request - t.ts_begin_host_send_request)
+    d["tpu_communication"]  = float(t.ts_begin_return - t.ts_end_submission)
+    d["tpu_return"]         = float(t.ts_end_return - t.ts_end_submission)
+    d["inference"]          = float(t.ts_begin_return - t.ts_end_submission)
+    d["total"]              = float(t.ts_end_return - t.ts_begin_host_send_request)
 
-    for k in d["usb"]:
-        if d["usb"][k] < 0:
+    for k in d:
+        if d[k] < 0:
             return {}
 
-    d["send"] = d["usb"]["host_communication"] + d["usb"]["host_submission"]
-    d["recv"] = d["usb"]["tpu_return"]
-    d["inf"]  = d["usb"]["inference"]
+    d["send"] = d["host_communication"] + d["usb"]["host_submission"]
+    d["recv"] = d["tpu_return"]
 
     return d
 
