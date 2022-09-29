e4e8df1670bfe667e822dd4ed22c5b2e061c9ec1
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-09-29 14:40:09 +0200

    Added args to test script

--- benchmarking/scripts/test.py
+++ benchmarking/scripts/test.py
@@ -1,14 +1,10 @@
 import sys
 sys.path[0]="/home/tensorDSE" # need to overwrite working directory, so imports can work
 
+import argparse
 from utils.model import Model
 from deploy import TPUDeploy
 from analysis import AnalyzeModelResults
-
-from main import log
-from utils.log import Log
-
-log = Log("results/debug.log")
 
 model="models/compiled/quant_CONV_2D_edgetpu.tflite"
 count=1
@@ -19,5 +15,23 @@ results["gpu"]   = []
 results["tpu"]   = []
 results["count"] = count
 
-results["tpu"].append(TPUDeploy(Model(model, "tpu"), count))
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+
+    parser.add_argument(
+        "-c", "--count", type=int, default=1, help="Number of times to run inference."
+    )
+
+    parser.add_argument(
+        "-t", "--timeout", type=int, default=30, help="Number of seconds to wait timeout."
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+args = get_args()
+results["tpu"].append(TPUDeploy(Model(model, "tpu"), args.count, args.timeout))
 # AnalyzeModelResults("TEST", results)
