34340d35cc02a61c33eb24e8d8776bbf32645e96
Alex Hoffman <alxhoff@gmail.com>
2023-03-24 16:59:00 +0100

    Run config output added to verbose mode

--- DSE/TensorDSE/src/main/java/net/sf/opendse/TensorDSE/TensorDSE.java
+++ DSE/TensorDSE/src/main/java/net/sf/opendse/TensorDSE/TensorDSE.java
@@ -121,7 +121,7 @@ public class TensorDSE {
 	 * @return EvolutionaryAlgorithmModule
 	 */
 	private static EvolutionaryAlgorithmModule GetEAModule(Double crossover_rate,
-			int population_size, int parents_per_generation, int generations,
+			int population_size, int generations, int parents_per_generation,
 			int offsprings_per_generation) {
 
 		EvolutionaryAlgorithmModule ea = new EvolutionaryAlgorithmModule();
@@ -356,6 +356,11 @@ public class TensorDSE {
 								generations.add(gen);
 								parents_per_generation.add((int) (pr * ps));
 								offsprings_per_generation.add((int) (or * ps));
+
+								if (args_namespace.getBoolean("verbose"))
+									System.out.println(String.format(
+											"gens: %d, pop size: %d, cross: %f, parents: %f = %f, offspring: %f = %f",
+											gen, ps, cr, pr, pr * ps, or, or * ps));
 							}
 
 
