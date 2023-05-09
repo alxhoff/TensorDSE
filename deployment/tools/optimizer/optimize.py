7a60c86d792ce867d251b6445e81a7eed3cde93d
Alex Hoffman <alxhoff@gmail.com>
2023-05-09 15:43:27 +0200

    Moddified optimize.py to handle mappings coming in from model summary JSON

--- deployment/tools/optimizer/optimize.py
+++ deployment/tools/optimizer/optimize.py
@@ -1,7 +1,7 @@
 import os
 import json
 import urllib
-from utils import ParseArgs, LoggerInit, RunTerminalCommand, ReadCSV, ReadJSON, CopyFile, MoveFile, CompareKeys
+from utils import ParseArgs, LoggerInit, RunTerminalCommand, ReadJSON, CopyFile, MoveFile
 
 log = LoggerInit()
 WORK_DIR      = os.path.dirname(os.path.abspath(__file__))
@@ -142,12 +142,20 @@ class Submodel(Model):
     def __init__(self, source_model_json: dict):
         CopyFile(os.path.join(RESOURCES_DIR, "shell", "shell_model.json"),
                  os.path.join(SUB_DIR, "json", "shell_model.json"))
-        super().__init__(path_to_model=os.path.join(SUB_DIR, "json", "shell_model.json"), 
+        super().__init__(path_to_model=os.path.join(SUB_DIR, "json", "shell_model.json"),
                           schema_path=os.path.join(RESOURCES_DIR, "schema", "schema.fbs"))
         self.json = ReadJSON(self.paths["json"])
         self.source_model_json = source_model_json
 
-    def AddOps(self, ops_block):
+    def AddOps(self, layers):
+        """Adds the appropriate operations, specified by the given layers, to
+        the submodel in preparation for saving the submodel.
+
+        Args:
+            layers (_type_): List of layer mappings, ie. (layer index, layer
+            type, target hardware) tupples, that should be compiled into a submodel.
+        """
+
         #Read Main Graph
         source_graph = self.source_model_json["subgraphs"][0]
 
@@ -156,7 +164,7 @@ class Submodel(Model):
 
         #Add Operators from Main Graph and add them according to index op 
         new_ops = []
-        for op_index in ops_block:
+        for op_index in [layer[0] for layer in layers]:
             new_ops.append(source_graph["operators"][op_index])
 
         #Add the OperatorCodes of the Newly added Operators and update their opcode_index        
@@ -233,16 +241,27 @@ class Submodel(Model):
         self.json["subgraphs"][0]["inputs"]      =   new_inputs
         self.json["subgraphs"][0]["outputs"]     =   new_outputs
         self.json["subgraphs"][0]["operators"]   =   new_ops
-        
-        
+
+
         self.json["description"]                 =   new_description
         self.json["buffers"]                     =   new_buffers
 
         #Save Submodel Number
-        self.submodel_number = '_'.join([str(elem) for elem in ops_block])
-        
-    def Save(self, block_key: str, index: int):
-        submodel_filename = "submodel{0}_{1}_{2}.json".format(index, block_key.lower(), self.submodel_number)
+        self.submodel_number = '_'.join([str(layer[0]) for layer in layers])
+
+    def Save(self, target_hardware: str, sequence_index: int):
+        """Saves a submodel to a JSON file, labeled using the target hardware,
+        the index of the sequence, and the indexes of the layers (in their model)
+        that are contained withing the submodel.
+
+        Args:
+            target_hardware (str): Name of the hardware that the submodel is to
+            be executed on
+            sequence_index (int): The index at where the sequence appears in the
+            set of sequences to be run on the respective hardware
+        """
+
+        submodel_filename = "submodel{0}_{1}_{2}.json".format(sequence_index, target_hardware.lower(), self.submodel_number)
         submodel_filepath = os.path.join(SUB_DIR, "json", submodel_filename)
         MoveFile(self.paths["json"], submodel_filepath)
         self.paths["json"] = submodel_filepath
@@ -251,14 +270,17 @@ class Submodel(Model):
 
 class Optimizer:
     def __init__(self, source_model_path, mapping_path):
+        import json
+
         log.info("Initializing Environment ...")
         self.InitializeEnv(source_model_path, mapping_path)
         log.info("Initializing Source Model ...")
         self.source_model = Model(self.source_model_path, self.schema_path)
         log.info("Source Model saved under: {}".format(os.path.join(MODELS_DIR, "source", "tflite")))
         log.info("Reading Mapping ...")
-        self.mapping = ReadCSV(self.mapping_path)
-        
+        with open(self.mapping_path, 'r') as f:
+            self.mapping = json.load(f)
+
     def CheckSchema(self):
         log.info("Checking schema ...")
         self.schema_path = os.path.join(RESOURCES_DIR,"schema","schema.fbs")
@@ -272,19 +294,23 @@ class Optimizer:
 
     def InitializeEnv(self, source_model_path, mapping_path):
         self.CheckSchema()
-        os.mkdir(MODELS_DIR)
+        if not os.path.exists(MODELS_DIR):
+            os.makedirs(MODELS_DIR)
         for directory in ["source", "sub", "final"]:
             sub_dir = os.path.join(MODELS_DIR, directory)
-            os.mkdir(sub_dir)
+            if not os.path.exists(sub_dir):
+                os.mkdir(sub_dir)
             for ext in ["tflite", "json"]:
                 ext_dir = os.path.join(sub_dir, ext)
-                os.mkdir(ext_dir)
+                if not os.path.exists(ext_dir):
+                    os.mkdir(ext_dir)
 
         model_filename = source_model_path.split("/")[len(source_model_path.split("/"))-1]
         self.source_model_path = os.path.join(SOURCE_DIR, "tflite", model_filename)
         CopyFile(source_model_path, self.source_model_path)
-        
-        os.mkdir(MAPPING_DIR)
+
+        if not os.path.exists(MAPPING_DIR):
+            os.mkdir(MAPPING_DIR)
         mapping_filename = mapping_path.split("/")[len(mapping_path.split("/"))-1]
         self.mapping_path = os.path.join(MAPPING_DIR, mapping_filename)
         CopyFile(mapping_path, MAPPING_DIR)
@@ -299,68 +325,113 @@ class Optimizer:
             dirs_to_clean.append(MAPPING_DIR)
 
         for directory in dirs_to_clean:
-            if os.path.isdir(directory): 
-                RunTerminalCommand("rm", "-rf", directory)   
+            if os.path.isdir(directory):
+                RunTerminalCommand("rm", "-rf", directory)
 
-    def AnalyseMapping(self):
+    def AnalyseMappings(self) -> None:
+        """ Parses the parsed JSON model summary containing the mappings to create
+        a summarized list per model containing where each list contains a
+        tupple of layer index in the model, layer type (eg. "conv_2d), and
+        the device name of where the layer should be mapped (eg. "cpu1").
+        """
+
         log.info("Analysing Mapping ...")
-        self.final_mapping = {}
-        sequence = {
-            "NEW": True,
-            "IN" : False,
-            "END": False
-        }
-        sequence_tracker = dict.fromkeys(HW_ID, 0)
-        for i, op in enumerate(self.mapping):
-            current_hw = HW_ID[op[1]]
-            if sequence["NEW"]:
-                sequence_tracker[current_hw]+=1
-                self.final_mapping[current_hw+str(sequence_tracker[current_hw])] = [i]
-                sequence["NEW"] = False
-                sequence["IN"] = True
-            elif sequence["IN"]:
-                self.final_mapping[current_hw+str(sequence_tracker[current_hw])].append(i)
 
-            if (i+1 == len(self.mapping)):
-                sequence["NEW"] = False
-                sequence["IN"]  = False
-                sequence["END"] = True
-            else:
-                sequence["END"] = (op[1] != self.mapping[i+1][1])
-                if sequence["END"]:
-                    sequence["NEW"] = True
-                    sequence["IN"]  = False
-        
-        for e in self.final_mapping:
-            ops = self.final_mapping[e]
-            log.info("        Operations : " + ", ".join(str(op) for op in ops))
-            log.info("        Target HW  : " + e[:3])
-        
-        final_mapping_path = os.path.join(RESOURCES_DIR, "final_mapping.json")
-        with open(final_mapping_path, "w") as fout:
-            json.dump(self.final_mapping, fout, indent=2)
+        self.final_mapping = []
 
-        log.info("Final Mapping Saved!\n")
+        for model in self.mapping:
+            model_layers = []
+            self.final_mapping.append(model_layers)
+            for i, layer in enumerate(model["layers"]):
+                model_layers.append((i, layer["type"], layer["mapping"]))
 
-    def ReadSourceModel(self):
+        for i, model in enumerate(self.final_mapping):
+            for j, layer in enumerate(model):
+                log.info("Model #{}, layer #{} mapped to {}".format(i, j, layer))
+
+    def CreateSubmodelLayerSequences(self) -> None:
+        """ From the mappings created by AnalyseMappings, sequential layers
+        that are mapped to the same hardware device are grouped to create
+        layer sequences that will later be compiled into submodels for execution.
+        This is done on a per-model basis.
+        """
+
+        self.model_layer_sequences = []
+
+        for model in self.final_mapping:
+            layer_seuqences = []
+            self.model_layer_sequences.append(layer_seuqences)
+
+            prev_layers_hardware = None
+            current_sequence = []
+
+            for layer in model:
+
+                if prev_layers_hardware is not None and prev_layers_hardware != layer[2]:
+                    layer_seuqences.append(current_sequence)
+                    current_sequence = []
+
+                current_sequence.append(layer)
+                prev_layers_hardware = layer[2]
+
+            layer_seuqences.append(current_sequence)
+
+
+    def ReadSourceModel(self) -> None:
         log.info("Converting Source Model from TFLite to JSON ...")
         self.source_model.Convert("tflite", "json")
         log.info("OK\n")
 
-    def CreateSubmodels(self):
-        for i, block_key in enumerate(self.final_mapping.keys()):
-            log.info("Initializing Shell Model for Submodel {0} ...".format(str(i)))
-            submodel = Submodel(self.source_model.json)
-            log.info("OK")
-            log.info("Adding Operations (" + ", ".join(str(op) for op in self.final_mapping[block_key]) + ") to Shell Model ...")
-            submodel.AddOps(self.final_mapping[block_key])
-            log.info("OK")
-            log.info("Saving Submodel {0} | Operations: {1} | Target HW: {2} ...".format(str(i), ", ".join(str(op) for op in self.final_mapping[block_key]), block_key))
-            submodel.Save(block_key, i)
-            log.info("OK")
-            log.info("Converting Submodel {0} from JSON to TFLite ...".format(str(i)))
-            submodel.Convert("json", "tflite")
-            log.info("OK\n")
+    def CompileAndSaveSubmodel(self, layer_sequence, model_index, sequence_index) -> None:
+        """Compiles a submodel from a sequence of layers
+
+        Args:
+            layer_sequence (list(tupples)): A list containing the layers that should
+            be compiled into the submodel.
+            model_index (int): Index showing which model in the list of all models
+            is currently being compiled.
+            sequence_index (int): The index at where the sequence appears in the
+            set of sequences to be run on the respective hardware.
+        """
+
+        log.info("Initializing shell model for submodel {} from model {}...".format(sequence_index, model_index))
+        submodel = Submodel(self.source_model.json)
+        log.info("OK")
+        log.info("Adding Operations (" + ", ".join(op[2] for op in layer_sequence) + ") to Shell Model ...")
+        submodel.AddOps(layer_sequence)
+        log.info("OK")
+        log.info("Saving Model {} | Submodel {} | Operations: {} | Target HW: {} ...".format(
+            model_index, sequence_index, ", ".join([str(layer[2]) for layer in layer_sequence]), layer_sequence[0][2]))
+        submodel.Save(layer_sequence[0][2], sequence_index)
+        log.info("OK")
+        log.info("Converting Submodel {0} from JSON to TFLite ...".format(str(sequence_index)))
+        submodel.Convert("json", "tflite")
+        log.info("OK\n")
+
+    def CreateSubmodels(self, sequences=False):
+        """ Create the individual submodels for either sequences of sequential
+        layers that are executed on the same hardware unit or for individual layers
+
+        Args:
+            sequences (bool, optional): If sequences should be compiled together
+            or if individual layers should be compiled into submodels alone.
+            Defaults to False.
+        """
+
+        # If we want to compile sequential layers mapped onto the same hardware
+        # into single inference sessions
+        if sequences:
+            for i, model in enumerate(self.model_layer_sequences):
+                for j, sequence in enumerate(model):
+                    self.CompileAndSaveSubmodel(layer_sequence=sequence,
+                                                model_index=i, sequence_index=j)
+        # Else each individual layer will be run in its own inference session
+        # which gives clearer overhead vs. execution time numbers
+        else:
+            for i, model in enumerate(self.final_mapping):
+                for j, layer in enumerate(model):
+                    self.CompileAndSaveSubmodel(layer_sequence=[layer],
+                                                model_index=i, sequence_index=j)
 
     def CompileForEdgeTPU(self):
         docker = Docker("debian-docker")
@@ -374,7 +445,8 @@ class Optimizer:
         del docker
 
     def Run(self):
-        self.AnalyseMapping()
+        self.AnalyseMappings()
+        self.CreateSubmodelLayerSequences()
         self.ReadSourceModel()
         self.CreateSubmodels()
         self.CompileForEdgeTPU()
