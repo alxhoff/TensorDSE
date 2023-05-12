20d5f50beb997b634942efe728379a58155ed7c4
Ala Fnayou <afnayou@gmail.com>
2023-05-12 11:01:59 +0200

    added some includes

---
+++ deployment/cpp_backend/include/tflite_utils.h
@@ -0,0 +1,34 @@
+#ifndef _TFLITE_UTILS_H
+#define _TFLITE_UTILS_H
+
+#include <vector>
+#include <stdint.h>
+#include <string>
+
+#include "tensorflow/lite/core/c/common.h"
+
+std::vector<uint8_t> decode_bmp(const uint8_t* input, int row_size, int width,
+                                int height, int channels, bool top_down);
+
+std::vector<uint8_t> read_bmp(const std::string& input_bmp_name, int* width,
+                            int* height, int* channels);
+
+int32_t ToInt32(const char p[4]);
+
+std::vector<uint8_t> ReadBmpImage(const char* filename,
+                                int* out_width = nullptr,
+                                int* out_height = nullptr,
+                                int* out_channels = nullptr);
+
+std::vector<std::string> ReadLabels(const std::string& filename);
+
+std::string GetLabel(const std::vector<std::string>& labels, int label);
+
+std::vector<float> Dequantize(const TfLiteTensor& tensor);
+
+std::vector<std::pair<int, float>> Sort(const std::vector<float>& scores,
+                                        float threshold);
+
+std::unique_ptr<tflite::FlatBufferModel> LoadModelFile(std::string filepath);
+
+#endif
