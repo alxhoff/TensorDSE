37f54c564f11ecb9b50f0c5b10e53ea17a901988
Alex Hoffman <alxhoff@gmail.com>
2023-08-23 10:53:31 +0200

    tmp

--- utils/usb/detect_tpu_bus.py
+++ utils/usb/detect_tpu_bus.py
@@ -6,8 +6,7 @@ def main() -> int:
 
     try:
         bus = re.findall('\/sys\/bus\/usb\/devices\/([0-9]+)-[0-9]+\/idVendor:.+', 
-                subprocess.check_output('grep 18d2 /sys/bus/usb/devices/*/idVendor', shell=True).decode('utf-8'))[0]
-                # subprocess.check_output('grep 18d1 /sys/bus/usb/devices/*/idVendor', shell=True).decode('utf-8'))[0]
+                subprocess.check_output('grep 18d1 /sys/bus/usb/devices/*/idVendor', shell=True).decode('utf-8'))[0]
     except Exception:
         bus = re.findall('\/sys\/bus\/usb\/devices\/([0-9]+)-[0-9]+\/idVendor:.+', 
             subprocess.check_output('grep 1a6e /sys/bus/usb/devices/*/idVendor', shell=True).decode('utf-8'))[0]
