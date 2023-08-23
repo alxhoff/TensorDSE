bd97fa32b580becdea3450471bf876cb88705fe8
Alex Hoffman <alxhoff@gmail.com>
2023-08-23 10:45:50 +0200

    tmp

---
+++ utils/usb/detect_tpu_bus.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+def main() -> int:
+    import subprocess, re
+
+    bus = re.findall('\/sys\/bus\/usb\/devices\/([0-9]+)-[0-9]+\/idVendor:.+', 
+            subprocess.check_output('grep 18d1 /sys/bus/usb/devices/*/idVendor', shell=True).decode('utf-8'))[0]
+
+    if bus == []:
+        re.findall('\/sys\/bus\/usb\/devices\/([0-9]+)-[0-9]+\/idVendor:.+', 
+            subprocess.check_output('grep 1a6e /sys/bus/usb/devices/*/idVendor', shell=True).decode('utf-8'))[0]
+
+    return bus
+
+if __name__ == "__main__":
+    sys.exit(main())
+
