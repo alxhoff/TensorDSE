a97d99b303dd4aebad1c1c9452107c241aa70749
Ala <afnayou@gmail.com>
2023-08-30 16:23:42 +0200

    adjusted usb analysis to support raspberry pi, model splitter supports multiple models

--- utils/analysis/__init__.py
+++ utils/analysis/__init__.py
@@ -14,7 +14,7 @@ def AnalyzeModelResults(parent_model:str, models_dict:Dict, hardware_summary:Dic
     from utils.usb.process import process_streams
     from utils.analysis.analysis import Analyzer
     import os
-    
+
     RESULTS_FOLDER = os.path.join(os.getcwd(), "resources/profiling_results")
     results_path = os.path.join(RESULTS_FOLDER, f"{parent_model}.json")
     print("Results file: {} from {}".format(results_path, os.getcwd()))
@@ -26,14 +26,14 @@ def AnalyzeModelResults(parent_model:str, models_dict:Dict, hardware_summary:Dic
         os.mkdir(RESULTS_FOLDER)
         if not os.path.isdir(RESULTS_FOLDER):
             sys.exit(-1)
-    
+
     data = {
             "models": [{
                 "name"      : parent_model,
                 "runs"      : models_dict["count"],
                 "layers"    : []
-        }]
-    }
+                }]
+            }
 
     # layers
     from itertools import groupby
@@ -43,19 +43,19 @@ def AnalyzeModelResults(parent_model:str, models_dict:Dict, hardware_summary:Dic
     for layer_name, layer in models_dict.items():
 
         layer_dict = {
-            "name"                  : layer_name,
-            "path"                  : {},
-            "delegates"             : []
-            }
+                "name"                  : layer_name,
+                "path"                  : {},
+                "delegates"             : []
+                }
 
         for delegate_name, delegate in layer.items():
 
             a = Analyzer(delegate.results, find_distribution=True)
 
             delegate_dict = {
-                "device"                      : delegate_name,
-                "count"                       : hardware_summary["{}_count".format(delegate_name.upper())],
-                "input"                     : {
+                    "device"                      : delegate_name,
+                    "count"                       : hardware_summary["{}_count".format(delegate_name.upper())],
+                    "input"                     : {
                         "shape"   : delegate.input_shape,
                         "type"    : delegate.input_datatype
                         },
@@ -83,9 +83,9 @@ def AnalyzeModelResults(parent_model:str, models_dict:Dict, hardware_summary:Dic
         for delegate in list(set(delegates) - set(layer.keys())):
 
             delegate_dict = {
-                "device"                    : delegate,
-                "count"                     : 0,
-            }
+                    "device"                    : delegate,
+                    "count"                     : 0,
+                    }
             layer_dict["delegates"].append(delegate_dict)
 
         data["models"][0]["layers"].append(layer_dict)
@@ -108,22 +108,22 @@ def MergeResults(parent_model:str, layers:dict, clean:bool=True):
             file = os.path.join(RESULTS_FOLDER, f"layer_{l.index}_{l.model_name}_{device.upper()}_USB.json")
             name = f"{l.model_name}_{l.index}"
             if (os.path.isfile(file) and name in names):
-                    j = load_json(file)
-                    for k,v in enumerate(d):
-                        delegates = [i["device"] for i in v["delegates"]]
-                        if (v["name"] == name):
-                            d[k]["path"][device] = j["path"][device]
-                            if device in delegates:
-                                for x,dele in enumerate(d[k]["delegates"]):
-                                    if dele["device"] == device:
-                                        d[k]["delegates"][x] = j["delegates"][0]
-                                        break
-                            else:
-                                d[k]["delegates"].append(j["delegates"][0])
-                            print(f"Merging {name} run on {device} onto the results from {parent_model}!")
+                j = load_json(file)
+                for k,v in enumerate(d):
+                    delegates = [i["device"] for i in v["delegates"]]
+                    if (v["name"] == name):
+                        d[k]["path"][device] = j["path"][device]
+                        if device in delegates:
+                            for x,dele in enumerate(d[k]["delegates"]):
+                                if dele["device"] == device:
+                                    d[k]["delegates"][x] = j["delegates"][0]
+                                    break
+                        else:
+                            d[k]["delegates"].append(j["delegates"][0])
+                        print(f"Merging {name} run on {device} onto the results from {parent_model}!")
 
-                    if clean:
-                        os.system(f"rm -f {file}")
+                if clean:
+                    os.system(f"rm -f {file}")
 
 
     data["models"][0]["layers"] = d
