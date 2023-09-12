813897ecedd7c8645faafe27e9e8225f0e344bf1
Alex Hoffman <alxhoff@gmail.com>
2023-09-12 11:37:29 +0200

    Modifying make and test scripts for final results

--- single_runs_GA.sh
+++ single_runs_GA.sh
@@ -16,5 +16,5 @@ for i in "$@"; do
 done
 
 for COUNT in 1 2 3 4 5; do
-    make run COUNT=20 MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$MODEL_SUMMARY.json" OUTPUT_FOLDER="../../resources/GA_results" OUTPUT_NAME="results_m$MODEL_SUMMARY_$COUNT.csv" ILP_MAPPING="false" POPULATION_SIZE=50 GENERATIONS=50
+    make nodeploy COUNT=20 MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$MODEL_SUMMARY.json" OUTPUT_FOLDER="../../resources/GA_results" OUTPUT_NAME="results_m$MODEL_SUMMARY_$COUNT.csv" ILP_MAPPING="false" POPULATION_SIZE=50 GENERATIONS=50
 done
