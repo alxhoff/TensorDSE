427a21b972f428127a027bd568b16a43498b5943
Alex Hoffman <alxhoff@gmail.com>
2023-09-20 16:52:23 +0200

    Adjusting test automation scripts

--- multi_runs_ILP.sh
+++ multi_runs_ILP.sh
@@ -2,6 +2,6 @@
 
 for COUNT in 1 2 3 4
 do
-    echo make nodeploy COUNT=20 MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$COUNT.json" OUTPUT_FOLDER="../../resources/ILP_results" OUTPUT_NAME="results_$COUNT.csv" ILP_MAPPING="true"
-    make nodeploy COUNT=20 MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$COUNT.json" OUTPUT_FOLDER="../../resources/ILP_results" OUTPUT_NAME="results_$COUNT.csv" ILP_MAPPING="true" VERBOSE="true"
+    echo make nodeploy USBMON=0 COUNT=20 MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$COUNT.json" OUTPUT_FOLDER="../../resources/ILP_results" OUTPUT_NAME="results_$COUNT.csv" ILP_MAPPING="true"
+    make nodeploy USBMON=0 COUNT=20 MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$COUNT.json" OUTPUT_FOLDER="../../resources/ILP_results" OUTPUT_NAME="results_$COUNT.csv" ILP_MAPPING="true" VERBOSE="true"
 done
