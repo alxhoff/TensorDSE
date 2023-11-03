5503360061f58f5bc18e05b207c7871827c4bc5a
Ala <afnayou@gmail.com>
2023-11-03 14:09:06 +0100

    minor changes in shell scripts for ilp and ga

--- multi_runs_GA_rpi.sh
+++ multi_runs_GA_rpi.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 PROFILING_RESULTS="../../resources/profiling_results/rpi"
-MODEL="MNIST_full_quanitization.json"
-OUTPUT_FOLDER="../../resources/GA_results"
+OUTPUT_FOLDER="../../resources/ILP_results"
+
+MODEL="resources/models/example_models/MNIST/MNIST_full_quanitization.tflite"
+file_basename="${MODEL%.*}"
+
+model_type="MNIST"
 
 # GENERATIONS=20
 # POPULATION=50
@@ -18,8 +22,8 @@ OUTPUT_FOLDER="../../resources/GA_results"
 GENERATIONS=20
 POPULATION=100
 
-for COUNT in 3 4; do
-    MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$COUNT.json"
+for COUNT in 4 5 6 7; do
+    MODEL_SUMMARY="../../resources/model_summaries/example_summaries/${model_type}/${file_basename}_multi_${COUNT}.json"
     OUTPUT_NAME="results_rpi_P$POPULATION\_G$GENERATIONS\_$COUNT.csv"
     make dse USBMON=0 MODEL=$MODEL PROFILING_COSTS=$PROFILING_RESULTS MODEL_SUMMARY=$MODEL_SUMMARY \
         OUTPUT_FOLDER=$OUTPUT_FOLDER OUTPUT_NAME=$OUTPUT_NAME POPULATION_SIZE=$POPULATION GENERATIONS=$GENERATIONS \
--- multi_runs_ILP_rpi.sh
+++ multi_runs_ILP_rpi.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 PROFILING_RESULTS="../../resources/profiling_results/rpi"
-MODEL="MNIST_full_quanitization.json"
 OUTPUT_FOLDER="../../resources/ILP_results"
 
-for COUNT in 6; do
-    MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$COUNT.json"
+MODEL="resources/models/example_models/MNIST/MNIST_full_quanitization.tflite"
+file_basename="${MODEL%.*}"
+
+model_type="MNIST"
+
+
+for COUNT in 1 2 3; do
+    MODEL_SUMMARY="../../resources/model_summaries/example_summaries/${model_type}/${file_basename}_multi_${COUNT}.json"
     OUTPUT_NAME="results_rpi_$COUNT.csv"
-    make dse MODEL=$MODEL PROFILING_COSTS=$PROFILING_RESULTS MODEL_SUMMARY=$MODEL_SUMMARY \
+    make dse USBMON=0 MODEL=$MODEL PROFILING_COSTS=$PROFILING_RESULTS MODEL_SUMMARY=$MODEL_SUMMARY \
         OUTPUT_FOLDER=$OUTPUT_FOLDER OUTPUT_NAME=$OUTPUT_NAME ILP_MAPPING="true" VERBOSE="true"
     done
