68fbde21fef0e9c01cd6e48af718fcc9f124e142
Ala <afnayou@gmail.com>
2023-11-05 15:42:08 +0100

    minor changes in entry and multi_runs scripts

--- multi_runs_GA_rpi.sh
+++ multi_runs_GA_rpi.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-PROFILING_RESULTS="../../resources/profiling_results/rpi"
-OUTPUT_FOLDER="../../resources/ILP_results"
+MODEL="resources/models/example_models/MNIST/MNIST_extended_full_quanitization.tflite"
+filename_with_extension="${MODEL##*/}"
+file_basename="${filename_with_extension%.*}"
 
-MODEL="resources/models/example_models/MNIST/MNIST_full_quanitization.tflite"
-file_basename="${MODEL%.*}"
+model_type="MNIST_extended"
 
-model_type="MNIST"
+platform="rpi"
+
+OUTPUT_FOLDER="resources/ILP_results/${file_basename}"
+if [ ! -d "$OUTPUT_FOLDER" ]; then
+    mkdir -p "$OUTPUT_FOLDER"
+fi
 
 # GENERATIONS=20
 # POPULATION=50
@@ -23,9 +28,9 @@ GENERATIONS=20
 POPULATION=100
 
 for COUNT in 4 5 6 7; do
-    MODEL_SUMMARY="../../resources/model_summaries/example_summaries/${model_type}/${file_basename}_multi_${COUNT}.json"
-    OUTPUT_NAME="results_rpi_P$POPULATION\_G$GENERATIONS\_$COUNT.csv"
-    make dse USBMON=0 MODEL=$MODEL PROFILING_COSTS=$PROFILING_RESULTS MODEL_SUMMARY=$MODEL_SUMMARY \
+    MODEL_SUMMARY="resources/model_summaries/example_summaries/${model_type}/${file_basename}_multi_${COUNT}.json"
+    OUTPUT_NAME="results_${platform}_multi_${COUNT}.csv"
+    make dse USBMON=0 MODEL=$MODEL MODEL_SUMMARY=$MODEL_SUMMARY \
         OUTPUT_FOLDER=$OUTPUT_FOLDER OUTPUT_NAME=$OUTPUT_NAME POPULATION_SIZE=$POPULATION GENERATIONS=$GENERATIONS \
         ILP_MAPPING="false"
     done
--- multi_runs_ILP_rpi.sh
+++ multi_runs_ILP_rpi.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
-PROFILING_RESULTS="../../resources/profiling_results/rpi"
-OUTPUT_FOLDER="../../resources/ILP_results"
+MODEL="resources/models/example_models/MNIST/MNIST_extended_full_quanitization.tflite"
+filename_with_extension="${MODEL##*/}"
+file_basename="${filename_with_extension%.*}"
 
-MODEL="resources/models/example_models/MNIST/MNIST_full_quanitization.tflite"
-file_basename="${MODEL%.*}"
+model_type="MNIST_extended"
 
-model_type="MNIST"
+platform="rpi"
+
+OUTPUT_FOLDER="resources/ILP_results/${file_basename}"
+if [ ! -d "$OUTPUT_FOLDER" ]; then
+    mkdir -p "$OUTPUT_FOLDER"
+fi
 
 
-for COUNT in 1 2 3; do
-    MODEL_SUMMARY="../../resources/model_summaries/example_summaries/${model_type}/${file_basename}_multi_${COUNT}.json"
-    OUTPUT_NAME="results_rpi_$COUNT.csv"
-    make dse USBMON=0 MODEL=$MODEL PROFILING_COSTS=$PROFILING_RESULTS MODEL_SUMMARY=$MODEL_SUMMARY \
+for COUNT in 3; do
+    MODEL_SUMMARY="resources/model_summaries/example_summaries/${model_type}/${file_basename}_multi_${COUNT}.json"
+    OUTPUT_NAME="results_${platform}_multi_${COUNT}.csv"
+    make dse USBMON=0 MODEL=$MODEL PLATFORM=$platform MODEL_SUMMARY=$MODEL_SUMMARY \
         OUTPUT_FOLDER=$OUTPUT_FOLDER OUTPUT_NAME=$OUTPUT_NAME ILP_MAPPING="true" VERBOSE="true"
+    MAPPINGS_FILE="${OUTPUT_FOLDER}/mappings.json"
+    NEW_MAPPINGS_FILE="${OUTPUT_FOLDER}/results_${platform}_multi_${COUNT}_mappings.json"
+
+    # Check if MAPPINGS_FILE exists and is a valid file path
+    if [ -f "$MAPPINGS_FILE" ]; then
+        # If it exists, then move it
+        mv "$MAPPINGS_FILE" "$NEW_MAPPINGS_FILE"
+        echo "Moved $MAPPINGS_FILE to $NEW_MAPPINGS_FILE"
+    else
+        echo "The file $MAPPINGS_FILE does not exist."
+    fi
     done
