057b9f48d77c5b3f2494d3e5825de55f23d15b30
Alex Hoffman <alxhoff@gmail.com>
2023-11-06 16:36:17 +0100

    Updated run scripts

--- single_runs_GA.sh
+++ single_runs_GA.sh
@@ -1,9 +1,48 @@
 #!/bin/bash
 
+MODEL_SUMMARY_FOLDER="../../resources/model_summaries/example_summaries"
+MODEL_SUMMARY_NAME="MLperf_ad_kws_resnet.json"
+PLATFORM="rpi"
+PROFILING_COSTS_FOLDER="../../resources/profiling_results"
+OUTPUT_FOLDER="../../resources/GA_results"
+OUTPUT_NAME="results_single_run.csv"
+POPULATION_SIZE=100
+GENERATIONS=20
+COUNT=20
+VERBOSE="true"
+
 for i in "$@"; do
     case $i in
-    -m=* | --MODEL_SUMMARY=*)
-        MODEL_SUMMARY="${i#*=}"
+    -p=* | --POPULATION=*)
+        POPULATION="${i#*=}"
+        shift # past argument=value
+        ;;
+    -g=* | --GENERATIONS=*)
+        GENERATIONS="${i#*=}"
+        shift # past argument=value
+        ;;
+    -m=* | --MODEL_SUMMARY_NAME=*)
+        MODEL_SUMMARY_NAME="${i#*=}"
+        shift # past argument=value
+        ;;
+    -o=* | --OUTPUT_FOLDER=*)
+        OUTPUT_FOLDER="${i#*=}"
+        shift # past argument=value
+        ;;
+    -p=* | --PROFILING_COSTS=*)
+        PROFILING_COSTS="${i#*=}"
+        shift # past argument=value
+        ;;
+    -l=* | --PLATFORM=*)
+        PLATFORM="${i#*=}"
+        shift # past argument=value
+        ;;
+    -d=* | --COUNT=*)
+        COUNT="${i#*=}"
+        shift # past argument=value
+        ;;
+    -d=* | --VERBOSE=*)
+        VERBOSE="${i#*=}"
         shift # past argument=value
         ;;
     --default)
@@ -15,6 +54,7 @@ for i in "$@"; do
     esac
 done
 
-for COUNT in 1 2 3 4 5; do
-    make nodeploy COUNT=20 MODEL_SUMMARY="../../resources/model_summaries/example_summaries/MNIST/MNIST_multi_$MODEL_SUMMARY.json" OUTPUT_FOLDER="../../resources/GA_results" OUTPUT_NAME="results_m$MODEL_SUMMARY_$COUNT.csv" ILP_MAPPING="false" POPULATION_SIZE=50 GENERATIONS=50
-done
+MODEL_SUMMARY="$MODEL_SUMMARY_FOLDER/$MODEL_SUMMARY_NAME"
+PROFILING_COSTS="$PROFILING_COSTS_FOLDER/$PLATFORM"
+
+make dse USBMON=0 COUNT=$COUNT MODEL_SUMMARY=$MODEL_SUMMARY OUTPUT_FOLDER=$OUTPUT_FOLDER PROFILING_COSTS=$PROFILING_COSTS OUTPUT_NAME=$OUTPUT_NAME ILP_MAPPING="false" POPULATION_SIZE=$POPULATION_SIZE GENERATIONS=$GENERATIONS VERBOSE=$VERBOSE
